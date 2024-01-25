1a0879619f625d98f8ca11eab525c8ccc1884fc3
진혁 <kitty0888@naver.com>
2024-01-26 01:26:30 +0900

    예외 설정..

---
+++ src/main/java/org/project/donkey/commons/exceptions/BadRequestException.java
@@ -0,0 +1,17 @@
+package org.project.donkey.commons.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public class BadRequestException extends org.project.donkey.commons.exceptions.CommonException {
+
+    public BadRequestException(Map<String, List<String>> messages) {
+        super(messages, HttpStatus.BAD_REQUEST);
+    }
+
+    public BadRequestException(String message) {
+        super(message, HttpStatus.BAD_REQUEST);
+    }
+}
